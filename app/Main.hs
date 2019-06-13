5f4b9135cd39c812e22d46ec6eedb6d379f502c9
Artem Chernyak <artem.chernyak@ocelotconsulting.com>
2019-06-12 20:55:02 -0500

    initial commit

---
+++ app/Main.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Control.Monad (replicateM)
+import Control.Monad.IO.Class (liftIO)
+import qualified Data.ByteString.Char8 as BC
+import Data.Text.Encoding (decodeUtf8, encodeUtf8)
+import qualified Data.Text.Lazy as TL
+import qualified Database.Redis as R
+import Network.URI (URI, parseURI)
+import qualified System.Random as SR
+import Web.Scotty
+
+alphaNum :: String
+alphaNum = ['A' .. 'Z'] ++ ['0' .. '9']
+
+randomElement :: String -> IO Char
+randomElement xs = do
+  let maxIndex :: Int
+      maxIndex = length xs - 1
+  randomDigit <- SR.randomRIO (0, maxIndex)
+  return (xs !! randomDigit)
+
+shortyGen :: IO [Char]
+shortyGen = replicateM 7 (randomElement alphaNum)
+
+saveURI ::
+     R.Connection
+  -> BC.ByteString
+  -> BC.ByteString
+  -> IO (Either R.Reply R.Status)
+saveURI conn shortURI uri = R.runRedis conn $ R.set shortURI uri
+
+getURI ::
+     R.Connection -> BC.ByteString -> IO (Either R.Reply (Maybe BC.ByteString))
+getURI conn shortURI = R.runRedis conn $ R.get shortURI
+
+main :: IO ()
+main = print "test"
